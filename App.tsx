71aa67c61b654c0d623a131349d0bc019b333f7a
Elia Morini <elia.morini@mikami.com>
2022-06-26 02:27:01 +0200

    chore(Style): add styledComponents and conf

--- App.tsx
+++ App.tsx
@@ -10,9 +10,30 @@
 
 import React from 'react';
 import Routing from './src/Routing/Routing';
+import {ThemeProvider} from '@emotion/react';
+import ReusableProvider from './src/context/ReusableProvider';
+import useGitRepoCheck from './src/hooks/useGitRepoCheck';
+import {GitRepoCheckContext} from './src/context/contexts';
+
+export const ReusableContext = React.createContext<any | {}>({});
+
+const theme = {
+  colors: {
+    primary: 'black',
+    secondary: 'white',
+    error: 'red',
+    success: 'green',
+  },
+};
 
 function App() {
-  return <Routing />;
+  return (
+    <ThemeProvider theme={theme}>
+      <ReusableProvider hook={useGitRepoCheck} Context={GitRepoCheckContext}>
+        <Routing />
+      </ReusableProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
