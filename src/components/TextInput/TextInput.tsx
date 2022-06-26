2b7360eef2242ef0b95878901c2057c386a0663b
Elia Morini <elia.morini@mikami.com>
2022-06-26 14:12:54 +0200

    feat(Components): add TextInput

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,30 @@
+import React, {FC} from 'react';
+import styled from '@emotion/native';
+import {space, SpaceProps, FontSizeProps, fontSize} from 'styled-system';
+
+const Input = styled.TextInput<SpaceProps & FontSizeProps>`
+  border-bottom-width: 2px;
+  border-bottom-color: ${({theme}) => theme.light.colors.primary};
+  ${space}
+  ${fontSize}
+`;
+
+interface Props {
+  onChange: (value: string) => void;
+  placeholder?: string;
+  value?: string;
+}
+
+const TextInput: FC<Props> = ({onChange, placeholder, value}) => {
+  return (
+    <Input
+      p={2}
+      fontSize={16}
+      onChangeText={onChange}
+      placeholder={placeholder}
+      value={value}
+    />
+  );
+};
+
+export default TextInput;
